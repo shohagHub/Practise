ba2b5cbebe905840f74ca48d8fd3f2e701081742
Saleh Enam Shohag <shohag402@gmail.com>
2016-01-17 22:09:06 +0600

    [Title] Test Commit; Signed of by: shohag402@gmail.com

---
+++ AcmPractise/469.c
@@ -0,0 +1,99 @@
+/*
+ * 469.c
+ *
+ *  Created on: Oct 12, 2015
+ *      Author: shohag
+ */
+#include <stdio.h>
+
+#define SIZE 101
+char Graph[SIZE][SIZE];
+int Visited[SIZE][SIZE];
+
+int N, T;
+int Ans;
+
+void readCase(){
+    N = 0;
+    while(gets(Graph[N])){
+        if(Graph[N][0] != 'W' && Graph[N][0] != 'L')
+            break;
+        N++;
+    }
+
+}
+
+void resetVisited(){
+    Ans = 0;
+    int i, j;
+    for(i = 0; i < N; i++)
+        for(j =  0; j < N; j++)
+            Visited[i][j] = 0;
+}
+
+void fill(int x, int y){
+    Visited[x][y] = 1;
+    Ans++;
+    if(x - 1 >= 0 && y - 1 >= 0 && Graph[x - 1][y - 1] == 'W' && !Visited[x - 1][y - 1])
+        fill(x -1, y - 1);
+    if(x - 1 >= 0 && Graph[x - 1][y] == 'W' && !Visited[x -1][y])
+        fill(x - 1, y);
+    if(x - 1 >= 0 && y + 1 < N && Graph[x - 1][y + 1] == 'W' && !Visited[x - 1][y + 1])
+        fill(x -1, y + 1);
+    if(y - 1 >= 0 && Graph[x][y - 1] == 'W' && !Visited[x][y - 1])
+        fill(x, y - 1);
+    if(y + 1 < N && Graph[x][y + 1] == 'W' && !Visited[x][y + 1])
+        fill(x, y + 1);
+    if(x + 1 < N && y - 1 >= 0 && Graph[x + 1][y - 1] == 'W' && !Visited[x + 1][y - 1])
+        fill(x + 1, y - 1);
+    if(x + 1 < N && Graph[x + 1][y] == 'W' && !Visited[x + 1][y])
+        fill(x + 1, y);
+    if(x + 1 < N && Graph[x + 1][y + 1] == 'W' && !Visited[x + 1][y + 1])
+        fill(x + 1, y + 1);
+}
+
+void solveCase(){
+
+    resetVisited();
+    int i, x, y;
+    i = x = y = 0;
+    while(Graph[N][i] != ' '){
+
+        x = x * 10 + Graph[N][i] - '0';
+        i++;
+    }
+    i++;
+    while(Graph[N][i] != '\0'){
+        y = y * 10 + Graph[N][i] - '0';
+        i++;
+    }
+    x--;
+    y--;
+    fill(x, y);
+    printf("%d", Ans);
+
+}
+
+void printCase(){
+    int i, j;
+    for(i = 0; i < N; i++)
+    {
+        for(j = 0; j < N; j++){
+            printf("%d\t", Graph[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+int main(){
+    scanf("%d", &T);
+    getchar();
+    getchar();
+    while(T--){
+        readCase();
+//        printCase();
+        solveCase();
+    }
+    return 0;
+}
