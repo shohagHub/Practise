674f00ba143ff64ba839ca95d95ad77069d6379c
Saleh Enam Shohag <shohag402@gmail.com>
2016-02-06 03:21:10 +0600

    [ACM & GIT] New file created for commit testing
    Signed-off-by: Saleh Enam Shohag <shohag402@gmail.com>

---
+++ AcmPractise/Shohag.java
@@ -0,0 +1,5 @@
+public class Shohag{
+    public static void main(){
+        System.out.println("This is test file");
+    }
+} 
---
+++ AcmPractise/test.c
@@ -0,0 +1,2 @@
+1. This is the testing file.
+2. I will use this file as the commit testing.
