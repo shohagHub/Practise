94124ae249da25b253b9e410e2bd84dbd933aa9b
Saleh Enam Shohag <shohag402@gmail.com>
2016-09-25 02:57:52 +0600

    Update 10276.c

--- AcmPractise/10276.c
+++ AcmPractise/10276.c
@@ -8,16 +8,33 @@
 
 #include <stdio.h>
 #include <math.h>
-#define SIZE 51
+#define SIZE 2501
 int output[SIZE];
 int used[SIZE];
 int noOfPegs;
+int limit;
+
+
+void reset(){
+	int i;
+	for(i = 0; i < SIZE; i++){
+		used[i] = 0;
+		output[i] = 0;
+	}
+}
+
+void coutAns(){
+	int i;
+	for(i = 1; used[i]; i++);
+
+	printf("%d\n", i - 1);
+}
 
 int isSquare(int n){
-    int h = n & 0xF;  // h is the last hex "digit"
+    int h = n & 0xF;
     if (h > 9)
         return 0;
-    // Use lazy evaluation to jump out of the if statement as soon as possible
+
     if (h != 2 && h != 3 && h != 5 && h != 6 && h != 7 && h != 8)
     {
         int t = (int) floor( sqrt((double) n) + 0.5 );
@@ -26,51 +43,59 @@ int isSquare(int n){
     return 0;
 }
 
+
 void print(){
     int i;
-    for(i = 0; output[i]; i++){
+    for(i = 1; output[i]; i++){
         printf("%d ", output[i]);
         output[i] = 0;
     }
+
     printf("\n");
 }
 
-void solve(int level){
-    if(level == noOfPegs){
-        print();
-        return;
-    }
-    int i;
-    for(i = 1; i < noOfPegs * noOfPegs; i++){
-        if(level == 0){
-            if(!used[i]){
-                used[i] = 1;
-                output[level] = i;
-                solve(level + 1);
-            }
-                
-        }
-        else{
-            if(!used[i] && output[level - 1] && isSquare(output[level - 1] + i)){
-                used[i] = 1;
-                output[level] = i;
-                solve(level + 1);
-            }
-        }
-    }
-    print();
+
+int solve2(){
+	if(noOfPegs == 1){
+		used[1] = 1;
+		return 1;
+	}
+	int index = 1;
+	int i;
+	int first = 1;
+	for(i = 1; i < noOfPegs * noOfPegs && index <= limit; i++){
+
+		if(!used[i] && first == 1){
+			first = 0;
+			used[i] = 1;
+			output[index++] = i;
+		}
+		else if(!used[i] && output[index - 1] && isSquare(output[index - 1] + i)){
+			used[i] = 1;
+			output[index++] = i;
+		}
+	}
+	if(index - 1 < limit)
+		limit = index - 1;
+
+	return index - 1;
 }
 
 int main(int argc, const char *argv[]){
-    freopen("/Users/naziaafroz/Desktop/10276/10276/10276.txt", "r", stdin);
+    /*freopen("10272.txt", "r", stdin);*/
     setbuf(stdout, NULL);
-    
     int T;
     scanf("%d", &T);
     while (T--) {
+    	reset();
         scanf("%d", &noOfPegs);
-//        printf("%d %d\n", noOfPegs, isSquare(noOfPegs));
-        solve(0);
+        limit = noOfPegs;
+        int i;
+        for(i = 0; i < noOfPegs; i++){
+        	solve2();
+        }
+        coutAns();
+
     }
     return 0;
 }
