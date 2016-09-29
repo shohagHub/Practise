837019c60c8fbbcf7f2da06c92d6150dd5e3a614
Saleh Enam Shohag <shohag402@gmail.com>
2016-09-30 01:50:36 +0600

    uva 10452 solved but wrong answer in submission..

--- AcmPractise/10452.c
+++ AcmPractise/10452.c
@@ -1,60 +1,97 @@
-//
-//  10452.c
-//  10276
-//
-//  Created by Nazia Afroz on 9/26/16.
-//  Copyright © 2016 Nazia Afroz. All rights reserved.
-//
-
 #include <stdio.h>
 #define SIZE 8
+int T;
+int M, N, hashIndex;
 char input[SIZE][SIZE];
-int m, n;
-int hashIndex;
-char output[SIZE][SIZE];
 char order[SIZE];
+int output[SIZE];
 
-void init(){
-    
-    order[1] = 'I';
-    order[2] = 'E';
-    order[3] = 'H';
-    order[4] = 'O';
-    order[5] = 'V';
-    order[6] = 'A';
-    order[7] = '#';
+void reset(){
+	int i;
+	for(i = 0; i < SIZE; i++)
+		output[i] = 0;
+}
+
+void initCase(){
+	order[0] = '@';
+	order[1] = 'I';
+	order[2] = 'E';
+	order[3] = 'H';
+	order[4] = 'O';
+	order[5] = 'V';
+	order[6] = 'A';
+	order[7] = '#';
+}
+
+void printCase(){
+	int i;
+
+	for(i = 0; i < 7; i++){
+/*printf("%d ", output[i]);*/
+		if(output[i] == 1)
+			printf("left");
+		if(output[i] == 2)
+			printf("forth");
+		if(output[i] == 3)
+			printf("right");
+		if(i != 6)
+			printf(" ");
+	}
+	printf("\n");
 }
 
 void findHash(){
     int i;
-    for (i = 0; i < n; i++)
-        if(input[m - 1][i] == '@')
+    for (i = 0; i < N; i++)
+        if(input[M - 1][i] == '@')
             hashIndex = i;
-    
+
 }
 
-void solve(int level, int x, int y){
-    if(level == 8){
-        return;
-    }
-    /*left*/
-    if(y > 0 && input[x][y - 1] == order[level]){
-        solve(level + 1, x, y);
-    }
-    
+void solve(int level, int i, int j){
+	if(order[level] != input[i][j])
+		return;
+	if(order[level] == '#'){
+		printCase();
+		return;
+	}
+	if(j > 0){
+		output[level] = 1;
+		solve(level + 1, i, j - 1);
+	}
+	if(j < N - 1){
+		output[level] = 3;
+		solve(level + 1, i, j + 1);
+	}
+	if(i > 0){
+		output[level] = 2;
+		solve(level + 1, i - 1, j);
+	}
 }
+
+
+
+
+void solveCase(){
+	solve(0, M - 1, hashIndex);
+}
+
 
 int main(){
-    freopen("/Users/naziaafroz/Desktop/10276/10276/10276.txt", "r", stdin);
-    int T;
-    scanf("%d", &T);
-    while (T--) {
-        scanf("%d%d", &m, &n);
-        int i;
-        for(i = 0; i < m; i++)
-            scanf("%s", input[i]);
-        findHash();
-        
-    }
-    return 0;
+	freopen("10452.txt", "r", stdin);
+	scanf("%d", &T);
+	int i;
+	initCase();
+	for(i = 0; i < T; i++){
+		scanf("%d%d", &M, &N);
+		int j;
+		for(j = 0; j < M; j++){
+			scanf("%s", input[j]);
+		}
+		findHash();
+		solveCase();
+	}
+	printf("\n");
+	return 0;
+
 }
